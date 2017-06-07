37913bf7ba9a0e1bcf0c675977b9d5fbd10eba2f
Guillaume <guillaume.normand@epita.fr>
2017-06-07 15:56:38 +0200

    add profile page

--- frontend/src/containers/profile-view/index.jsx
+++ frontend/src/containers/profile-view/index.jsx
@@ -1,11 +1,56 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import StatusBar from "../common/statusBar.jsx";
+import { getUserById } from "../../actions/authentication.js";
+import { getBoardByOwnerId, getBoardSharedWithUser } from "../../actions/board.js";
 
 class ProfileView extends Component {
 
   constructor(props) {
     super(props);
+	
+	this.state = {
+		username: "",
+		email: "",
+		nbAuthoredBoard: 0,
+		nbPersonalBoard: 0,
+		nbSharedBoard:0
+	};
+  }
+  
+  componentWillMount() {
+	this.getBoards();
+  }
+  
+  getUserInfo() {
+   
+  }
+  
+  getBoards() {
+	let user = getUserById(this.props.userid, this.props.token);
+    if (user) {
+	   let name = JSON.parse(user).username;
+	   let mail = JSON.parse(user).email;
+	   this.setState({email: mail});
+       this.setState({username: name});
+	   
+	   let author = getBoardByOwnerId(this.props.userid);
+	   if (author) {
+	     let tab = JSON.parse(author);
+	     let own = tab.filter((elt) => elt.authorizedUsers.length > 0);
+	     let perso = tab.filter((elt) => elt.authorizedUsers.length == 0);
+	   
+	     this.setState({nbAuthoredBoard: own.length});
+	     this.setState({nbPersonalBoard: perso.length});
+	
+	   }
+	   console.log("name :");
+	   console.log(name);
+	   let shared = getBoardSharedWithUser(name);
+	   if (shared) {
+		 this.setState({nbSharedBoard: JSON.parse(shared).length});
+	   }
+	} 
   }
 
   render() {
@@ -16,7 +61,12 @@ class ProfileView extends Component {
         top: "0px",
         position: "absolute",
         width: "100%"
-      }
+      },
+	  infoView: {
+		left: "20px",
+        position: "relative",
+        width: "100%"
+	  }
     };
 
     if (this.props.token === null)
@@ -25,7 +75,14 @@ class ProfileView extends Component {
     return (
       <div style={styles.profileView}>
         <StatusBar dispatch={this.props.dispatch}/>
-        Profile page
+		<div style={styles.infoView}>
+			<h1>Profile page</h1>
+			<h4>username : {this.state.username}</h4>
+			<h4>email : {this.state.email}</h4>
+			<h4>Shared boards you own : {this.state.nbAuthoredBoard}</h4>
+			<h4>Shared boards you contribute to : {this.state.nbSharedBoard}</h4>
+			<h4>Personal boards : {this.state.nbPersonalBoard}</h4>
+		</div>
       </div>
     );
   }
